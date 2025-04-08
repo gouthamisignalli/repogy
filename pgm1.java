82af3e29eb111e7d4808ddad4c0800a8eac70059
username <gouthami@gmail.com>
2025-04-09 04:34:57 +0530

    hello

---
+++ pgm1.java
@@ -0,0 +1,36 @@
+public class pgm1{
+    
+        public static void main(String[] args) {
+            
+            int number = 10;  
+            System.out.println("Integer: " + number);
+    
+            double pi = 3.14159;  
+            System.out.println("Double: " + pi);
+    
+            
+            char letter = 'A';  
+            System.out.println("Character: " + letter);
+    
+            
+            boolean isJavaFun = true;  
+            System.out.println("Boolean: " + isJavaFun);
+    
+            
+            String greeting = "Hello, Java!";  
+            System.out.println("String: " + greeting);
+    
+            
+            long largeNumber = 10000000000L;  
+            System.out.println("Long: " + largeNumber);
+    
+           
+            float temperature = 36.6f;  
+            System.out.println("Float: " + temperature);
+        }
+    }
+    
+    
+
+    
+
